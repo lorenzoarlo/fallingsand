8e615238413879cad053de98ec8f73fe12eb42ca
pierror404 <pierluca.pevere@gmail.com>
2026-02-03 01:32:05 +0100

    pusho algoritmo buono

---
+++ src/cuda/other-algorithm/kernels.cuh
@@ -0,0 +1,16 @@
+#ifndef CUDA_KERNELS
+#define CUDA_KERNELS
+
+#include "../../simulation.h"
+
+#define BLOCK_SIZE 16
+
+#define SAND_NOISE_CHANCE 0.4f
+#define WATER_FALL_DOWN_CHANCE 0.9f
+#define WATER_FALL_DENSITY_CHANCE 1.0f
+#define WATER_MOVE_DIAGONAL_CHANCE 0.5f
+#define WATER_MOVE_HORIZONTAL_CHANCE 0.8f
+
+__global__ void kernel(unsigned char* grid_in, unsigned char* grid_out, int width, int height, int offset_x, int offset_y, int generation);
+
+#endif
