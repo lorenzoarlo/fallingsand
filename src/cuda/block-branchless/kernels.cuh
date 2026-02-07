577ff6cc33a4462ecf59649bf28a9f948d97c64a
infoPier <pierluca.pevere@gmail.com>
2026-02-07 10:56:51 +0100

    thank you, nsight compute

--- src/cuda/block-branchless/kernels.cuh
+++ src/cuda/block-branchless/kernels.cuh
@@ -3,7 +3,7 @@
 
 #include "../../simulation.h"
 
-#define BLOCK_SIZE 32
+#define BLOCK_SIZE 16
 
 __global__ void kernel_opt(unsigned char* __restrict__ grid_in, unsigned char* __restrict__ grid_out, int width, int height, int generation);
 
